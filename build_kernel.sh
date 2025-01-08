d9e9b0a49d2596ade10bd92163466fa7e763bfab
Mohamed4k <aaamohammed603@gmail.com>
2025-01-08 23:24:23 +0200

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,7 +2,6 @@
 # copy right by zetaxbyte
 # you can rich me on telegram t.me/@zetaxbyte
 # flags of proton clang
-sudo ln -sf /usr/bin/python2.7 /usr/local/bin/python2
 sudo apt-get install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang
 
 sudo apt-get install gcc-aarch64-linux-gnu
