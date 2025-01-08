f4b906e5d730294e77bedc3d74c9b4b468c32924
Mohamed4k <aaamohammed603@gmail.com>
2025-01-08 14:30:27 +0200

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -6,7 +6,7 @@
 sudo apt-get install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang
 
 sudo apt-get install gcc-aarch64-linux-gnu
-
+sudo apt-get install gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu                                                                                                                                               
 
 if [ -d $(pwd)/../proton-clang ] ; then
 echo -e "\n lets's go \n"
