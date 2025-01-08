0d0e7e42161c496e13441b177c7ad6d6650a2354
Mohamed4k <aaamohammed603@gmail.com>
2025-01-08 10:07:42 +0200

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,6 +3,9 @@
 sudo apt-get install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang
 sudo apt update
 sudo apt install python2
+pkg install python2
+apt install python -y
+apt install python
 sudo apt-get install gcc-aarch64-linux-gnu
 
 git clone  https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc
