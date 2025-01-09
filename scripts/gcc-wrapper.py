4147c97ebfbc007bac7f78b91f75344db3bba744
Joel Gómez <nahuelgomez329@gmail.com>
2025-01-09 14:27:02 +0000

    scripts: update gcc-wrapper.py for Python 3
    Signed-off-by: Joel Gómez <nahuelgomez329@gmail.com>

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -1,8 +1,31 @@
-#! /usr/bin/env python2
-# SPDX-License-Identifier: GPL-2.0-only
-# Copyright (c) 2011-2017, 2018 The Linux Foundation. All rights reserved.
-
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2011-2016, The Linux Foundation. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of The Linux Foundation nor
+#       the names of its contributors may be used to endorse or promote
+#       products derived from this software without specific prior written
+#       permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Invoke gcc, looking for warnings, and causing a failure if there are
 # non-whitelisted warnings.
@@ -17,10 +40,11 @@ import subprocess
 # force LANG to be set to en_US.UTF-8 to get consistent warnings.
 
 allowed_warnings = set([
-    "umid.c:138",
-    "umid.c:213",
-    "umid.c:388",
- ])
+    "core.c:144",
+    "inet_connection_sock.c:430",
+    "inet_connection_sock.c:467",
+    "inet6_connection_sock.c:89",
+])
 
 # Capture the name of the object file, can find it.
 ofile = None
@@ -31,7 +55,7 @@ def interpret_warning(line):
     line = line.rstrip('\n')
     m = warning_re.match(line)
     if m and m.group(2) not in allowed_warnings:
-        print("error, forbidden warning:", m.group(2))
+        print("error, forbidden warning:", m.group(2), file=sys.stderr)
 
         # If there is a warning, remove any object if it exists.
         if ofile:
@@ -42,35 +66,37 @@ def interpret_warning(line):
         sys.exit(1)
 
 def run_gcc():
-    args = sys.argv[1:]
-    # Look for -o
-    try:
-        i = args.index('-o')
-        global ofile
-        ofile = args[i+1]
-    except (ValueError, IndexError):
-        pass
+	args = sys.argv[1:]
+	if not args:
+		print("Error: No arguments provided to the script", file=sys.stderr)
+		sys.exit(1)
 
-    compiler = sys.argv[0]
+	# Look for -o
+	try:
+		i = args.index('-o')
+		global ofile
+		ofile = args[i + 1]
+	except (ValueError, IndexError):
+		pass
 
-    try:
-        proc = subprocess.Popen(args, stderr=subprocess.PIPE)
-        for line in proc.stderr:
-            if sys.version_info >= (3, 0):
-                line = line.decode()
-            print(line)
-            interpret_warning(line)
+	compiler = sys.argv[0]
 
-        result = proc.wait()
-    except OSError as e:
-        result = e.errno
-        if result == errno.ENOENT:
-            print(args[0] + ':' + e.strerror)
-            print('Is your PATH set correctly?')
-        else:
-            print(' '.join(args), str(e))
+	try:
+		proc = subprocess.Popen(args, stderr=subprocess.PIPE, universal_newlines=True)
+		for line in proc.stderr:
+			print(line, end='', file=sys.stderr)
+			interpret_warning(line)
 
-    return result
+		result = proc.wait()
+	except OSError as e:
+		result = e.errno
+		if result == errno.ENOENT:
+			print(args[0] + ':', e.strerror, file=sys.stderr)
+			print('Is your PATH set correctly?', file=sys.stderr)
+		else:
+			print(' '.join(args), str(e), file=sys.stderr)
+
+	return result
 
 if __name__ == '__main__':
     status = run_gcc()
